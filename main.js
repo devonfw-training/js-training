303df0df8b9a8fab1a616086e83ec5834932e8e7
Szewcow Tomasz <tomasz.szewcow.ext@adcubum.com>
2017-04-18 16:16:03 +0200

    promise exercise description added

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 console.log('hello from main js');
 
+// add acount number
 class Account {
     constructor(balance, currency) {
         this.balance = balance;
@@ -26,6 +27,12 @@ class Person {
         return this.accounts.filter(account => account.balance > 0);
     };
 
+    // add findAccount(accountNumber) method (use Array.find method)
+
+    // add withdraw(accountNumber, amount) method which returns promise (use Promise API -> new Promise(resolve, reject))
+    // promise is resolved after 3 seconds (use setTimeout(callback)) when the account is found and the amount >= account.balance
+    // promise is rejected either when account is not found or there are not enough funds on the found account
+
     _calculateBalance() {
         let totalBalance = 0;
 
@@ -37,9 +44,12 @@ class Person {
     };
 };
 
+// add account numbers to all Account constructors
 const person = new Person('John', 'Example', [new Account(1500, 'EUR')]);
 console.log(person.sayHello());
 person.addAccount(new Account(-2500, 'EUR'));
 console.log(person.sayHello());
 console.log(person.filterPositiveAccounts());
+
+// call person.withdraw for different cases
 
